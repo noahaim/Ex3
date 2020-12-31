9a34cb42c99192f7b508e14158c98bf0422432a5
avihugoren <73278156+avihugoren@users.noreply.github.com>
2020-12-31 16:02:06 +0200

    graph algo

---
+++ src/GraphAlgo.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from DiGraph import DiGraph
+from GraphAlgoInterface import GraphAlgoInterface
+
+
+class GraphAlgo(GraphAlgoInterface):
+    def load_from_json(self, file_name: str) -> bool:
+        pass
+
+    def save_to_json(self, file_name: str) -> bool:
+        pass
+
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        if self.my_graph.get_all_v().get(id1) is None or self.my_graph.get_all_v().get(id2) is None:
+            return (float('inf'), [])
+
+
+    def connected_component(self, id1: int) -> list:
+        pass
+
+    def connected_components(self) -> List[list]:
+        pass
+
+    def plot_graph(self) -> None:
+        pass
+
+    def __init__(self, graph=DiGraph()):
+        self.my_graph = graph
+
