e6528557793fc197bb086daed0fa15d4d113d4f6
noa haim <noa.202.r@gmail.com>
2021-01-01 13:51:39 +0200

    add dijkstra and shortest_path

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,16 +1,17 @@
 from typing import List
 from DiGraph import DiGraph
 from GraphAlgoInterface import GraphAlgoInterface
-
+from GraphInterface import GraphInterface
+from queue import PriorityQueue
+import heapq
 
 class GraphAlgo(GraphAlgoInterface):
 
-    def __init__(self, graph=DiGraph()):
+    def __init__(self, graph: DiGraph = DiGraph()):
         self.my_graph = graph
 
     def get_graph(self) -> GraphInterface:
-        return my_graph
-
+        return self.my_graph
 
     def load_from_json(self, file_name: str) -> bool:
         pass
@@ -18,9 +19,37 @@ class GraphAlgo(GraphAlgoInterface):
     def save_to_json(self, file_name: str) -> bool:
         pass
 
+    def dijkstra(self, src: int):
+        # update all th node in the graph to weight inf and parent none
+        for node in self.my_graph.get_all_v().values():
+            node.set_parent(None)
+            node.set_weight(float('inf'))
+        # short_path from key to himself is 0
+        self.my_graph.get_all_v().get(src).set_weight(0)
+        pq_help = []
+        heapq.heappush(pq_help, self.my_graph.get_all_v().get(src))
+        while pq_help:
+            temp_node = heapq.heappop(pq_help)
+            for key in self.my_graph.all_out_edges_of_node(temp_node.key).keys():
+                m = temp_node.weight + temp_node.edges_out[key]
+                if m < self.my_graph.get_all_v().get(key).weight:
+                    self.my_graph.get_all_v().get(key).set_weight(m)
+                    self.my_graph.get_all_v().get(key).set_parent(temp_node.key)
+                    heapq.heappush(pq_help, self.my_graph.get_all_v().get(key))
+                    heapq.heapify(pq_help)
+
     def shortest_path(self, id1: int, id2: int) -> (float, list):
         if self.my_graph.get_all_v().get(id1) is None or self.my_graph.get_all_v().get(id2) is None:
-            return (float('inf'), [])
+            return float('inf'), []
+        self.dijkstra(id1)
+        weight_path = self.my_graph.get_all_v().get(id2).weight
+        path = [id2]
+        parent = self.my_graph.get_all_v().get(id2).parent
+        while parent is not None:
+            path.append(parent)
+            parent = self.my_graph.get_all_v().get(parent).parent
+        path.reverse()
+        return weight_path, path
 
     def connected_component(self, id1: int) -> list:
         pass
@@ -30,4 +59,3 @@ class GraphAlgo(GraphAlgoInterface):
 
     def plot_graph(self) -> None:
         pass
-
--- src/MyNode.py
+++ src/MyNode.py
@@ -1,4 +1,4 @@
-from GeoLocation import GeoLocation
+
 
 class MyNode:
     def __init__(self, key: int, pos: tuple = None, tag: int = 0,
@@ -15,19 +15,32 @@ class MyNode:
         self.tag = tag
 
     def set_weight(self, weight: float = float('inf')):
-        self.weight = float('inf')
+        self.weight = weight
+
+    # def get_weight(self, key: int):
+    #     return self.weight
 
     def set_parent(self, parent: int = None):
         self.parent = parent
 
     def add_edges_in(self, edge_src: int, edge_weight: float):
-        self.edges_in [edge_src] = edge_weight
+        self.edges_in[edge_src] = edge_weight
 
     def add_edges_out(self, edge_dest: int, edge_weight: float):
-        self.edges_out [edge_dest] = edge_weight
+        self.edges_out[edge_dest] = edge_weight
 
     def remove_edges_in(self, edge_src: int):
         self.edges_in.__delitem__(edge_src)
 
     def remove_edges_out(self, edge_dest: int):
         self.edges_out.__delitem__(edge_dest)
+
+    # def __gt__(self, other):
+    #     return self.weight < other.weight
+    #
+    # def __eq__(self, other):
+    #     return self.weight == other.weight
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+
